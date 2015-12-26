3b385f3b8dbe6a3b6b45a0077c7c94bd699a85a3
tpalmer75 <taylor.hatch.palmer@gmail.com>
2015-12-26 13:16:11 -0700

    Initial commit
    USA Web components, Angular, JQuery

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// create the module and name it scotchApp
+// also include ngRoute for all our routing needs
+var lunchApp = angular.module('lunchApp', ['ngRoute']);
+
+// configure our routes
+lunchApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'templates/landing.html'
+        })
+        .when('/add-children', {
+            templateUrl : 'templates/add-children.html'
+        })
+        .when('/legal', {
+            templateUrl : 'templates/legal.html'
+        });
+
+});
+
+// create the controller and inject Angular's $scope
+lunchApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+lunchApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+lunchApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
